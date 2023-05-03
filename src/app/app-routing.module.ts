9491efb6af008e1d30958690aff90ecb1e1b7674
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2023-05-03 18:55:48 +0200

    start add form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddComponent } from './add/add.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent}
+  { path: '', component: DashboardComponent},
+  { path: 'addInvoice', component: AddComponent}
 ];
 
 @NgModule({
